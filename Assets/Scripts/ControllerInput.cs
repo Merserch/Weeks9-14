94fa7f8f123c0ebb4304af5e832c0d1d09acd809
merserch <merserch@OA-S141-07>
2026-03-11 12:04:15 -0400

    Did a bit more in the gym

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -5,13 +5,16 @@ public class ControllerInput : MonoBehaviour
 {
     public float speed = 5;
     public Vector2 movement;
+    public Vector2 rotation;
+    public Vector2 mouseMovement;
     public AudioSource SFX;
     
     private void Update()
     {
-        //transform.position += (Vector3)movement * speed * Time.deltaTime;
-
-        transform.position = movement;
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        
+        transform.rotation = Quaternion.Euler(0, 0, (rotation.y * 90) + (rotation.x * -180) );
+        //transform.position = mouseMovement;
 
     }
 
@@ -32,7 +35,13 @@ public class ControllerInput : MonoBehaviour
     public void OnPoint(InputAction.CallbackContext context)
     {
         //same as Mouse.current.position.ReadValue()
-        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+        //mouseMovement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
         
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //same as Mouse.current.position.ReadValue()
+        rotation = context.ReadValue<Vector2>();
     }
 }
