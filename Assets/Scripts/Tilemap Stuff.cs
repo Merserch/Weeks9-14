7f6db0feff82d560fa7c48a1bb773a085160c3d4
merserch <merserch@OA-S141-07>
2026-04-01 10:36:08 -0400

    Finished up the tilemap video

--- Assets/Scripts/Tilemap Stuff.cs
+++ Assets/Scripts/Tilemap Stuff.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
 
@@ -6,7 +7,9 @@ using UnityEngine.Tilemaps;
 public class TilemapStuff : MonoBehaviour
 {
     public Tilemap tilemap;
+    public Transform highlight;
 
+    public Tile flower;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +21,23 @@ public class TilemapStuff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Get the mouse position, and get the world point under that pixel
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //Asking what cell of the grid is under that position, and its giving me the bottom left corner of the cell
+        Vector3Int cellPos = tilemap.WorldToCell(mousePos);
+        //Asking what the center of that cell is in world space, and giving me that position
+        Vector3 pos = tilemap.GetCellCenterWorld(cellPos);
+
+        //Debug.Log(mousePos + " is over cell: " + cellPos);
+        //setting the boba position to the center of the cell that the mouse is over
+        highlight.position = pos;
+
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Debug.Log(tilemap.GetTile(cellPos));
+            tilemap.SetTile(cellPos, flower);
+        }
+
+
     }
 }
