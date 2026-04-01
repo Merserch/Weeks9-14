87c8dcb3543d609ec73109e7d036194e33dbfb85
merserch <merserch@OA-S141-07>
2026-04-01 09:31:46 -0400

    Going through the videos from this week

---
+++ Assets/Scripts/Tilemap Stuff.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+
+public class TilemapStuff : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
