b5467b5630d8400defa47a12977022dd654b392b
UMBREON\chaxl <merserch@sheridancollege.ca>
2026-04-09 19:21:11 -0400

    going to cry

---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+    public Transform handPos;
+    public Vector2 start;
+    public Vector2 end;
+    public AnimationCurve swinging;
+    public float attackRange = 1f;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float t = 0;
+
+        //while (t < attackRange)
+        //{
+        //    t += Time.deltaTime;
+            
+
+        //}
+    }
+
+
+        //IEnumerator OnPunch()
+        //{
+
+
+        //        yield return null;
+        //    }
+    
+}
+
